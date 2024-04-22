54648b8bacd462ea4a8c0d2860f18552eac84ed8
epili50 <epili50@gmail.com>
2024-04-22 21:46:04 +0200

    Realizado 1-c

--- ex1-c.js
+++ ex1-c.js
@@ -7,6 +7,6 @@ let peces = ["Dory", "Nemo", "Estrella"];
 /**
  * Modifica la primera posición del array 'peces'. Cambia a "Dory" por "Tory". 
  */
-
+peces[0]= "Tory";
 
 console.log(peces); // ["Tory", "Nemo", "Estrella"]
